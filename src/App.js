19b194be5336df2de4ce03813d7813eb672fc5e8
donjoe <giumontanaro@gmail.com>
2018-12-17 22:00:14 +0100

    USe Button instances inside App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import Controls from './components/Controls/Controls';
-import './App.css';
 import Display from './components/Display/Display';
+import Button from './components/Button/Button';
+import './App.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faPause } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlay, faPause);
+
+
 
 class App extends Component {
 
@@ -19,6 +29,8 @@ class App extends Component {
         <Controls label="session" value={this.state.sessionLength}/>
        </div>
        <Display mode={this.state.mode}/>
+       <Button type="start_stop"><FontAwesomeIcon icon="play" /></Button>
+       <Button type="reset">RESET</Button>
       </div>
     );
   }
