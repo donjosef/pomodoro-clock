367bd9e40573469621774d3813c0ed950b4af75f
donjoe <giumontanaro@gmail.com>
2018-12-18 18:13:12 +0100

    Display the remaining time on Display component

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function Display(props) {
-  const {mode} = props;
+  const {mode, seconds} = props;
+
+  const mins = Math.floor(seconds / 60);
+  const remainingSecs = Math.floor(seconds % 60);
+
   return (
     <div className="Display">
       <p id="timer-label">{mode}</p>
-      <div id="time-left">25000</div>
+      <div id="time-left">
+      {mins < 10 ? '0' + mins : mins}:{remainingSecs < 10 ? '0' + remainingSecs : remainingSecs}
+      </div>
     </div>
   );
 }
