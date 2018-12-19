099cc66d2b65ad2c0e3caf0cfa01b5c4be7dd271
donjoe <giumontanaro@gmail.com>
2018-12-19 12:30:00 +0100

    Updates

---
+++ precache-manifest.0785c075987eb86d08739fad5a704b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/pomodoro-clock/static/media/BeepSound.988833bf.wav"
+  },
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "988ba30a0a3c56f18ea3",
+    "url": "/pomodoro-clock/static/js/main.988ba30a.chunk.js"
+  },
+  {
+    "revision": "56c01979d08b49c42432",
+    "url": "/pomodoro-clock/static/js/1.56c01979.chunk.js"
+  },
+  {
+    "revision": "988ba30a0a3c56f18ea3",
+    "url": "/pomodoro-clock/static/css/main.605ae48e.chunk.css"
+  },
+  {
+    "revision": "f826f7d96687df1f36a3ea1597741d05",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
